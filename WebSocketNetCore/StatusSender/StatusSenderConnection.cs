d477c7972d56d33b09778d491dab6ff4838194d8
Braian Cariaga <cariaga.bh@gmail.com>
2017-10-26 23:52:18 -0300

    Add Sources

---
+++ WebSocketNetCore/StatusSender/StatusSenderConnection.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSocketNetCore.WebSocketManager;
+
+namespace WebSocketNetCore.StatusSender
+{
+    public class StatusSenderConnection : WebSocketConnection
+    {
+        public StatusSenderConnection(WebSocketHandler handler) : base(handler)
+        {
+        }
+
+        public override async Task ReceiveAsync(string message)
+        {
+            var receiveMessage = JsonConvert.DeserializeObject<ReceiveMessage>(message);
+
+            //aca se pueden definir los receptores, por ahora todos
+            var receivers = Handler.Connections.ToList();
+
+            if (receivers.Count > 0)
+            {
+                receivers.ForEach(async (websocketConnection) =>
+                {
+                    var sendMessage = JsonConvert.SerializeObject(new SendMessage
+                    {
+                        Sender = "Server",
+                        Message = receiveMessage.Message
+                    });
+
+                    await websocketConnection.SendMessageAsync(sendMessage);
+
+                });
+            }
+        }
+
+        private class ReceiveMessage
+        {
+            public string Receiver { get; set; }
+
+            public string Message { get; set; }
+        }
+
+        private class SendMessage
+        {
+            public string Sender { get; set; }
+
+            public string Message { get; set; }
+        }
+
+    }
+}
---
+++ WebSocketNetCore/StatusSender/StatusSenderHandler.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSocketNetCore.WebSocketManager;
+
+namespace WebSocketNetCore.StatusSender
+{
+    public class StatusSenderHandler : WebSocketHandler
+    {
+        protected override int BufferSize { get => 1024 * 4; }
+
+        public override async Task<WebSocketConnection> OnConnected(HttpContext context)
+        {
+            //Todo Usar algo asi com un token para validar la conexión
+            //var name = context.Request.Query["Token"];
+
+            //para filtar las conexiones
+            //var connections = Connections.ToList();
+
+            //connections.ForEach(async conn => 
+            //{
+            //    var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+            //    var senderClient = new StatusSenderConnection(this)
+            //    {
+            //        WebSocket = webSocket
+            //    };
+
+            //    Connections.Add(senderClient);
+            //});
+
+            var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+
+            var senderClient = new StatusSenderConnection(this)
+            {
+                WebSocket = webSocket
+            };
+
+            Connections.Add(senderClient);
+
+            //TODO: esto esta mal ya lo se
+            return null;
+        }
+    }
+}
